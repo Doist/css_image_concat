f70ed53bbd263476d51fce6561a1893fda28f79b
amix <amix@amix.dk>
2015-08-27 20:22:44 +0100

    Updated css image concat

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="css_image_concat",
     license="MIT",
-    version="2.0",
+    version="2.5",
     author="amix",
     author_email="amix@amix.dk",
     url="http://www.amix.dk/",
