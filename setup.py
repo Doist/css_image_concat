e5b005d0ec3ed01a3eab450397751b053a152266
amix <amix@amix.dk>
2015-09-03 15:43:43 +0100

    Use transparent bg when composing the images

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="css_image_concat",
     license="MIT",
-    version="2.5",
+    version="2.6",
     author="amix",
     author_email="amix@amix.dk",
     url="http://www.amix.dk/",
